0836f3384cddc90477a97d77355c94889670f6e3
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-09-14 22:04:49 +0100

    Update daily.js

--- daily.js
+++ daily.js
@@ -4,9 +4,9 @@ const { Resolver } = require('dns').promises;
 const resolver = new Resolver();
 resolver.setServers(['1.1.1.1', '8.8.8.8']);
 
+checker();
 
-
-async function main() {
+async function checker() {
 const response = await fetch('https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat');
 const body = await response.text();
 const TLDs = body.trim().split("\n").filter(line => !line.startsWith("//") && line !== "");
