e7d1942ff829f6106326ffba751ad2b2ef00462c
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-09-14 21:57:17 +0100

    Create daily.js

---
+++ daily.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch';
+
+const { Resolver } = require('dns').promises;
+const resolver = new Resolver();
+resolver.setServers(['1.1.1.1', '8.8.8.8']);
+
+const response = await fetch('https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat');
+const body = await response.text();
+const TLDs = body.trim().split("\n").filter(line => !line.startsWith("//") && line !== "");
+
+var newList = [];
+
+for (let TLD of TLDs) {
+  try {
+  let txt = await resolver.resolveTxt("_psl."+TLD);
+  newList.push(TLD);
+  } catch {
+    console.log("ERROR: "+tld);
+  } 
+}
+
+console.log(newList);
