70ad0e3392530a8893e2db74496b8db088629d8a
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-09-15 01:38:46 +0100

    Update daily.js

--- daily.js
+++ daily.js
@@ -1,18 +1,20 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fs = require('fs');
 var whois = require('whois')
 
-checker();
+updateList();
 
-async function checker() {
+async function updateList() {
 const response = await fetch('https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat');
 const body = await response.text();
 const TLDs = body.trim().split("\n").filter(line => !line.startsWith("//") && line !== "");
 
+var contents = "";
+
 for (let TLD of TLDs) {
   whois.lookup(TLD.replace("*.", ""), function(err, data) {
-	console.log(data)
+	if (data.includes("%ERROR:101: no entries found")) file += "\n"+TLD;
   });
 }
-
-
+fs.writeFileSync('invalid', contents);
 }
